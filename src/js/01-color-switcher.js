9bb26c36b14fcfbc9b934e8e32b30e402ac4b774
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-01-29 00:04:36 +0800

    color switch commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+import '../css/common.css';
+
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
+
+const randomBodyColorGenerator = {
+  DELAY: 1000,
+  intervalID: null,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  startInterval() {
+    this.intervalID = setInterval(() => {
+      body.style.backgroundColor = this.getRandomHexColor();
+    }, this.DELAY);
+  },
+
+  stopInterval() {
+    clearInterval(this.intervalID);
+  },
+
+  toggleButtons() {
+    startBtn.disabled = !startBtn.disabled;
+    stopBtn.disabled = !stopBtn.disabled;
+  },
+
+  start() {
+    startBtn.addEventListener('click', () => {
+      this.startInterval();
+      this.toggleButtons();
+    });
+
+    stopBtn.addEventListener('click', () => {
+      this.stopInterval();
+      this.toggleButtons();
+    });
+  },
+};
+
+randomBodyColorGenerator.start();
 
